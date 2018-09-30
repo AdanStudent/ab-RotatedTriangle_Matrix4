711b5225555eb129853fc079cc424da42c55d7e8
AdanStudent <adan.benitez@loop.colum.edu>
2018-09-30 10:36:40 -0500

    Finished HW

---
+++ ab-RotatedTriangle_Matrix4/RotatedTriangle_Matrix4.js
@@ -0,0 +1,96 @@
+﻿//Rotated Triangle Matrix.js
+//Vertexshader program
+var VSHADER_SOURCE =
+    //x' = x cos b - y sin b
+    //y' = x sin b + y cos b
+    //z' = z
+    'attribute vec4 a_Position;\n' +
+    'uniform float u_CosB, u_SinB;\n' +
+    'uniform mat4 u_xformMatrix;\n' +
+    'void main() {\n' +
+    ' gl_Position = u_xformMatrix * a_Position;\n' +
+    '}\n';
+
+// Fragment Shader program
+var FSHADER_SOURCE =
+    'void main() {\n' +
+    ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+    '}\n';
+
+//Rotation Angle
+var ANGLE = 90;
+
+function main() {
+    //Retreive <canvas>
+    var canvas = document.getElementById('webgl');
+
+    //get rendering context
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    //Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initalize shaders');
+        return;
+    }
+
+    //write the positions of vertices to a vertex shader
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('failed to set the positions of the vertices');
+        return;
+    }
+
+    var xformMatrix = new Matrix4();
+    //set the rotation matrix 
+    xformMatrix.setRotate(ANGLE, 0, 0, 1);
+
+    // Pass the rotation matrix to the vertex shader
+    var u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix');
+
+    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix.elements);
+
+    //specify the color for clearing <canvas>
+    gl.clearColor(0, 0, 0, 1);
+
+    //Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //draw the rectangle
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}//end of main
+
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([
+        0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+
+    var n = 3;
+
+    //create Buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('failed to create the buffer object');
+        return -1;
+    }
+
+    //bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    //write data into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('failed to get the storage location of a_Position');
+        return -1;
+    }
+    //assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    //enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
